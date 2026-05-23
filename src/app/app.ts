999a78d4de65ada89b949876f07b348e6651d424
Pranita More <morepranita915@gmail.com>
2026-05-23 15:16:08 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  // protected readonly title = signal('Pranitas Angular App');
 }
